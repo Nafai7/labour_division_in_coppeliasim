f3347c535d68cfff28be01a34ad7dd72377be75a
Nafai7 <nafailuet@gmail.com>
2022-11-06 15:52:17 +0100

    Changed addon script to external lua script file that will be accessed from the scene scripts in order to remove code repeating Added path following functions.

---
+++ LabourDivisionUtilities.lua
@@ -0,0 +1,19 @@
+function get_path_data_needed(path)
+    local pathData=sim.unpackDoubleTable(sim.readCustomDataBlock(path,'PATH'))
+    local m=Matrix(#pathData//7,7,pathData)
+    local pathPositions=m:slice(1,1,m:rows(),3):data()
+    local pathQuaternions=m:slice(1,4,m:rows(),7):data()
+    local pathLengths,totalLength=sim.getPathLengths(pathPositions,3)
+    return pathPositions, pathQuaternions, pathLengths, totalLength
+end
+
+function follow_path_step(path, drone, velocity, posAlongPath, previousSimulationTime, pathPositions, pathQuaternions, pathLengths, totalLength)
+    local t=sim.getSimulationTime()
+    posAlongPath=posAlongPath+velocity*(t-previousSimulationTime)
+    posAlongPath=posAlongPath % totalLength
+    local pos=sim.getPathInterpolatedConfig(pathPositions,pathLengths,posAlongPath)
+    local quat=sim.getPathInterpolatedConfig(pathQuaternions,pathLengths,posAlongPath,nil,{2,2,2,2})
+    sim.setObjectPosition(drone,path,pos)
+    sim.setObjectQuaternion(drone,path,quat)
+    return posAlongPath, t
+end
--- test.py
+++ test.py
@@ -9,26 +9,6 @@ droneInitMass = sim.getShapeMass(drone)
 sensor = sim.getObject('/Proximity_sensor')
 
 sim.startSimulation()
-messageToDisplay = 1
-while (sim.getSimulationTime() < 8):
-    sim.setShapeMass(drone, sim.getShapeMass(drone) + 0.001)
-    if (sim.getSimulationTime().is_integer()):
-        print("[DRON]: Bzzzz")
-        result = sim.checkProximitySensor(sensor, drone)
-        if (result != 0):
-            if (messageToDisplay == 1):
-                print("[SENSOR]: O jakiś dron")
-            if (messageToDisplay == 2):
-                print("[SENSOR]: O NIE O SPADA")
-            if (messageToDisplay == 3):
-                print("[SENSOR]: O BOŻE O KURWA!")
-            if (messageToDisplay == 4):
-                print("[SENSOR]: CO SIĘ DZIĘJE SŁODKI JEZU NIE WIEM CO MAM ROBIĆ POMOCY! KURWA POMOCY LUDZIE DRON SIĘ ROZJEBIE AAAA!")
-            messageToDisplay += 1
-        else:
-            print("[SENSOR]: Wszystko w porządku")
-    client.step()
 
-sim.setShapeMass(drone, droneInitMass)
 sim.stopSimulation()
 del client
